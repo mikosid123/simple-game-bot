93cfa1ee24af086799c9de6598e67f187876c654
mikosid123 <michael.avellana@gmail.com>
2024-05-05 22:41:21 +0800

    feat: add instruction to login

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import cv2 as cv
-import pyautogui as ui
-import numpy as np
+import pyautogui
+import numpy
+import time
 
 def main():
-  """
-  This function contains the core logic of your program.
-  """
-  # Add your program's code here
-  print("Hello, world!")
-
+  pyautogui.FAILSAFE = True;
+  time.sleep(3);
+  pyautogui.typewrite(['win', 'c', 'h', 'r', 'o', 'm', 'e', 'enter'], interval=0.2);
+  time.sleep(3);
+  pyautogui.typewrite('maroonstudios.hrhub.ph\n', interval=0.02);
+  time.sleep(5);
+  login_coordinate = pyautogui.locateCenterOnScreen('login_button.jpg');
+  pyautogui.leftClick(login_coordinate);
+  time.sleep(10);
+  clockin_dropdown_coordinate = pyautogui.locateCenterOnScreen('clockin_dropdown.jpg');
+  pyautogui.leftClick(clockin_dropdown_coordinate);
 if __name__ == "__main__":
-  main()
+  main();
