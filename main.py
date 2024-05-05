298f04576c982dfb3102a1fd5ceab23177b410de
mikosid123 <michael.avellana@gmail.com>
2024-05-05 15:41:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui as ui
+import numpy as np
+
+def main():
+  """
+  This function contains the core logic of your program.
+  """
+  # Add your program's code here
+  print("Hello, world!")
+
+if __name__ == "__main__":
+  main()
